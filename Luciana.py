e3f530689c7216cd5edb661dc27b5bc0b3e89e2c
lucianaoliveirapro <102829339+lucianaoliveirapro@users.noreply.github.com>
2022-04-22 09:38:32 -0300

    Add files via upload

---
+++ Luciana.py
@@ -0,0 +1,7 @@
+x=input("insira seu primeiro nome")
+y=input("insira seu salário do mês de abril")
+parte1y:[0:3]
+parte2y:[3:6]
+parte3y:[6:7]
+print=("o salário de "+ x + "foi de R$" + parte1y + parte2y + parte3y + no mês de abril)
+
