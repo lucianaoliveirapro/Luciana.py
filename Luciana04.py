f9625f02c061d9351202746547c30e1639664750
lucianaoliveirapro <102829339+lucianaoliveirapro@users.noreply.github.com>
2022-04-23 11:52:44 -0300

    Add files via upload

---
+++ Luciana04.py
@@ -0,0 +1,8 @@
+primeiroNome = input("digite seu primeiro nome: ")
+dia = input("dia do nascimento:")
+mês = input("Mês do nascimento:")
+ano = input("ano do nascimento:")
+
+mensagem = "{} nasceu no dia: {}/{}/{}"
+
+print (mensagem.format(primeiroNome, dia, mês, ano))
